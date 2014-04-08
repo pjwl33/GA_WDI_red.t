0b5c63d15957286b506e360361d72d8aa12811fc
pjwl33 <paul.leejw@gmail.com>
2014-04-08 16:21:48 -0400

    starting project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedT::Application.config.secret_key_base = '73c80723e3c7b067c51f05c576a62a903c2d864e45907e8a6f066290cf25c8f739dbf78b2228c7d4ff145db0d1d7a3fbe0593996c8e727d2432925a185d1c557'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RedT::Application.config.session_store :cookie_store, key: '_red_t_session'

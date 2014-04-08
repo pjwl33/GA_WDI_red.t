77f9b485e5ec0a1b79a1f6dfdaa7fa9956dea745
pjwl33 <paul.leejw@gmail.com>
2014-04-08 16:51:19 -0400

    post_spec and model added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
+  validates :url, :title, :user_id, presence: true
+  validates :url, uniqueness: true
+
 end
---
+++ spec/post_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Post do
+
+  it { should validate_presence_of(:url) }
+  it { should validate_presence_of(:title) }
+  it { should validate_uniqueness_of(:url)}
+  it { should belong_to(:user) }
+
+end
